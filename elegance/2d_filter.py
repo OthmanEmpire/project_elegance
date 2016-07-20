115a8009733d453dd328f691709d9d26e08433c5
sc14omsa <sc14omsa@leeds.ac.uk>
2016-07-20 09:51:33 +0100

    crawling but working first draft of the 2d_filter script

--- elegance/2d_filter.py
+++ elegance/2d_filter.py
@@ -1,26 +1,80 @@
-# A script that removes background noise from 2D images of C. elegans
-# @Author: Othman Alikhan
-# @Date: 2016 - 07 - 15
+"""
+###############################################################################
+# A script that suppresses all the information in the 2D images of C. Elegans #
+# except the worm itself (i.e. image filtering).                              #
+#                                                                             #
+#                                                                             #
+# Author: Othman Alikhan                                                      #
+# Email: sc14omsa@leeds.ac.uk                                                 #
+#                                                                             #
+# Python Version: 2.6.6                                                       #
+# Date Created: 2016-07-15                                                    #
+###############################################################################
+"""
 
-import numpy as np
-import cv2
 import os
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-def generateImagePath():
+
+def main():
     """
-    Generates the path absolute path to the first frame of the worm images.
+    Main execution point of the script.
+    """
+    # Control variables
+    frameStart = 1
+    frameDiff = 20
+    frameEnd = 700
+    framePause = 1000          # in milliseconds
+
+    # Initializing the graph
+    plt.title("C. Elegans in Action")
+    plt.xticks([]), plt.yticks([])
+    p = plt.imshow(calculateFrameDifference(frameStart, frameStart + frameDiff))
+
+    # Slideshow of the difference between two frames for all frames
+    for frame in range(frameStart, frameEnd):
+        p.set_data(calculateFrameDifference(frame, frame + frameDiff))
+        plt.pause(framePause/1000)
+
+
+#TODO:
+#    ani = animation.F
+
+
+def calculateFrameDifference(frameOne, frameTwo):
+    """
+    Calculates the absolute difference between the given two frames
+    numbers of the worm.
+
+    :param frameOne: Integer number of the first frame.
+    :param frameTwo: Integer number of the second frame.
+    :return: An image in openCV format.
+    """
+    img1 = cv2.imread(generateFramePath(frameOne))
+    img2 = cv2.imread(generateFramePath(frameTwo))
+    return cv2.absdiff(img1, img2)
+
+def generateFramePath(frameNum):
+    """
+    Generates the absolute path to a given frame number in the 2D images
+    directory for C. Elegans.
+
+    :param frameNum: The frame number of the 2D image.
     :return: A string of the absolute path to a worm image.
     """
-    return os.path.abspath(os.path.join("..", "assets", "images_2d", "frame_000001.png"))
+    dir = ["..", "assets", "images_2d"]
+    fileName = ["frame_%06d.png" % frameNum]
+    path = dir + fileName
 
+    relPath = os.path.join(*path)
+    absPath = os.path.abspath(relPath)
 
-# Load a colour image in grayscale
-path = generateImagePath()
+    if os.path.exists(absPath):
+        return absPath
+    else:
+        raise NameError("Could not generate a path for the give frame number!")
 
-img = cv2.imread(path, 0)
-
-cv2.imshow("image", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
+if __name__ == "__main__":
+    main()

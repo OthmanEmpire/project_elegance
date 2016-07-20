07e462d0012adfed0a319d081e396a138c496565
sc14omsa <sc14omsa@leeds.ac.uk>
2016-07-20 15:14:28 +0100

    added ImageFilter class to 2d_filter.py with functioning matplotlib

--- elegance/2d_filter.py
+++ elegance/2d_filter.py
@@ -11,70 +11,106 @@
 # Date Created: 2016-07-15                                                    #
 ###############################################################################
 """
-
 import os
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
-def main():
+class ImageFilter:
     """
-    Main execution point of the script.
+    Responsible for filtering out noise from the 2D images of C. Elegans.
     """
-    # Control variables
-    frameStart = 1
-    frameDiff = 20
-    frameEnd = 700
-    framePause = 1000          # in milliseconds
 
-    # Initializing the graph
-    plt.title("C. Elegans in Action")
-    plt.xticks([]), plt.yticks([])
-    p = plt.imshow(calculateFrameDifference(frameStart, frameStart + frameDiff))
+    def __init__(self, fStart, fEnd, fDiff, fPause):
+        """
+        A simple constructor.
 
-    # Slideshow of the difference between two frames for all frames
-    for frame in range(frameStart, frameEnd):
-        p.set_data(calculateFrameDifference(frame, frame + frameDiff))
-        plt.pause(framePause/1000)
+        :param fStart: The first frame of the worm images.
+        :param fEnd: The last frame of the worm images.
+        :param fDiff: Number of frames apart the two subtracted frames are.
+        :param fPause: Number of milliseconds between animated frames.
+        """
+        # Initializing variables
+        self.fStart = fStart
+        self.fEnd = fEnd
+        self.fDiff = fDiff
+        self.fPause = fPause
 
+        # Initializing the animation
+        self.fig = plt.figure()
+        self.im = plt.imshow(
+            self.computeFrameDifference(self.fStart, self.fStart + self.fDiff))
 
-#TODO:
-#    ani = animation.F
+    def runAnimation(self):
+        """
+        Runs the worm animation that is based on the animate function.
+        """
+        plt.title("C. Elegans in Action")
+        ani = animation.FuncAnimation(self.fig, self.animate,
+                                      frames=np.arange(fStart, fEnd),
+                                      fargs=(fDiff,),
+                                      interval=fPause,
+                                      blit=True)
+        plt.show()
 
+    def animate(self, frame, *args):
+        """
+        Used to draw the animation which shows the difference of two frames.
+        This is done via matplotlib's animation.FuncAnimation.
 
-def calculateFrameDifference(frameOne, frameTwo):
-    """
-    Calculates the absolute difference between the given two frames
-    numbers of the worm.
+        :param frame: The current frame to be animated.
+        :param args: Contains the 'fDiff' parameter which dictates how
+        many frames apart the two frames to be subtracted are.
+        :return: An AxesImage which is to be processed by
+        animation.FuncAnimation.
+        """
+        fDiff = args[0]
+        self.im.set_data(self.computeFrameDifference(frame, frame + fDiff))
+        return self.im,
 
-    :param frameOne: Integer number of the first frame.
-    :param frameTwo: Integer number of the second frame.
-    :return: An image in openCV format.
-    """
-    img1 = cv2.imread(generateFramePath(frameOne))
-    img2 = cv2.imread(generateFramePath(frameTwo))
-    return cv2.absdiff(img1, img2)
+    def computeFrameDifference(self, fOne, fTwo):
+        """
+        Calculates the absolute difference between the given two frames
+        numbers of the worm.
 
-def generateFramePath(frameNum):
-    """
-    Generates the absolute path to a given frame number in the 2D images
-    directory for C. Elegans.
+        :param fOne: Integer number of the first frame.
+        :param fTwo: Integer number of the second frame.
+        :return: An image in openCV format.
+        """
+        img1 = cv2.imread(self.generateFramePath(fOne))
+        img2 = cv2.imread(self.generateFramePath(fTwo))
+        return cv2.absdiff(img1, img2)
 
-    :param frameNum: The frame number of the 2D image.
-    :return: A string of the absolute path to a worm image.
-    """
-    dir = ["..", "assets", "images_2d"]
-    fileName = ["frame_%06d.png" % frameNum]
-    path = dir + fileName
+    def generateFramePath(self, fNum):
+        """
+        Generates the absolute path to a given frame number in the 2D images
+        directory for C. Elegans. This requires the frames to be named
+        chronologically using the form "frame_0000XY.png", where six digits (
+        can be leading zeros) are present after the frame prefix.
 
-    relPath = os.path.join(*path)
-    absPath = os.path.abspath(relPath)
+        :param fNum: The frame number of the 2D image.
+        :return: A string of the absolute path to a worm image.
+        """
+        dir = ["..", "assets", "images_2d"]
+        fileName = ["frame_%06d.png" % fNum]
+        path = dir + fileName
 
-    if os.path.exists(absPath):
-        return absPath
-    else:
-        raise NameError("Could not generate a path for the give frame number!")
+        relPath = os.path.join(*path)
+        absPath = os.path.abspath(relPath)
+
+        if os.path.exists(absPath):
+            return absPath
+        else:
+            raise NameError("Could not generate a path frame number %d!" % fNum)
 
 if __name__ == "__main__":
-    main()
+    # Control variables
+    fStart = 1
+    fDiff = 10
+    fEnd = 680
+    fPause = 1      # in milliseconds
+
+    imageFilter = ImageFilter(fStart, fEnd, fDiff, fPause)
+    imageFilter.runAnimation()

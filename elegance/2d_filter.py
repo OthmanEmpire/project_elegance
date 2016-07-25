141af46df0a47ce48ed94cfdb3d9b0627b4aa8ae
OthmanEmpire <oz.alikhan@gmail.com>
2016-07-25 14:16:34 +0100

    working subplots of diff and raw images (2d_filter)

--- elegance/2d_filter.py
+++ elegance/2d_filter.py
@@ -32,43 +32,92 @@ class ImageFilter:
         :param fDiff: Number of frames apart the two subtracted frames are.
         :param fPause: Number of milliseconds between animated frames.
         """
-        # Initializing variables
+        # Initializing instance variables
         self.fStart = fStart
         self.fEnd = fEnd
         self.fDiff = fDiff
         self.fPause = fPause
 
-        # Initializing the animation
+        # Initializing matplotlib figure
         self.fig = plt.figure()
-        self.im = plt.imshow(
-            self.computeFrameDifference(self.fStart, self.fStart + self.fDiff))
+        self.fig.canvas.set_window_title("I See Elegance?")
 
     def runAnimation(self):
         """
-        Runs the worm animation that is based on the animate function.
+        Runs all the worm animations which are based in the animate function.
         """
-        plt.title("C. Elegans in Action")
-        ani = animation.FuncAnimation(self.fig, self.animate,
-                                      frames=np.arange(fStart, fEnd),
+        ani = animation.FuncAnimation(self.fig,
+                                      self.updateAllAnimation,
+                                      init_func=self.initializeAllAnimation,
+                                      frames=np.arange(self.fStart, self.fEnd),
                                       fargs=(fDiff,),
                                       interval=fPause,
                                       blit=True)
         plt.show()
 
-    def animate(self, frame, *args):
+    def initializeAllAnimation(self):
         """
-        Used to draw the animation which shows the difference of two frames.
-        This is done via matplotlib's animation.FuncAnimation.
+        Initializes all animations.
 
-        :param frame: The current frame to be animated.
-        :param args: Contains the 'fDiff' parameter which dictates how
-        many frames apart the two frames to be subtracted are.
-        :return: An AxesImage which is to be processed by
-        animation.FuncAnimation.
+        :return: A tuple containing all the AxesImages to be initialized.
+        """
+        self.initializeRawAnimation()
+        self.initializeDifferenceAnimation()
+        return self.axesImgRaw, self.axesImgDif
+
+    def initializeRawAnimation(self):
+        """
+        Initializes the animation for the raw images.
+        """
+        self.axesRaw = self.fig.add_subplot(2, 2, 1)
+        self.axesRaw.set_title("Image Raw")
+
+        img = self.readFrame(self.fStart)
+        self.axesImgRaw = self.axesRaw.imshow(img)
+
+    def initializeDifferenceAnimation(self):
+        """
+        Initializes the animation for the difference of images.
+        """
+        self.axesDif = self.fig.add_subplot(2, 2, 3)
+        self.axesDif.set_title("Image Difference")
+
+        img = self.computeFrameDifference(self.fStart, self.fStart + self.fDiff)
+        self.axesImgDif = self.axesDif.imshow(img)
+
+    def updateAllAnimation(self, fNum, *args):
+        """
+        Updates all animations.
+
+        :param fNum: The current frame to be animated.
+        :param args: Contains the 'fDiff' parameter for the difference
+        animation. This is how many frames apart the subtraction should be.
+        :return: A tuple containing all the AxesImages to be animated.
         """
         fDiff = args[0]
-        self.im.set_data(self.computeFrameDifference(frame, frame + fDiff))
-        return self.im,
+        self.updateRawAnimation(fNum, self.axesImgRaw)
+        self.updateDifferenceAnimation(fNum, fDiff, self.axesImgDif)
+        return self.axesImgRaw, self.axesImgDif
+
+    def updateRawAnimation(self, fNum, axes):
+        """
+        Updates the animation that is responsible for showing the raw frames.
+
+        :param fNum: The frame number to be shown.
+        :param axes: The matplotlib axes to draw the animation on.
+        """
+        axes.set_data(self.readFrame(fNum))
+
+    def updateDifferenceAnimation(self, fNum, fDiff, axes):
+        """
+        Updates the animation that is responsible for computing the OpenCV
+        difference (and not OpenCV subtract!) between two frames.
+
+        :param fNum: The frame number of the first image to be subtracted.
+        :param fDiff: The difference between the first and next frame.
+        :param axes: The matplotlib axes to draw the animation on.
+        """
+        axes.set_data(self.computeFrameDifference(fNum, fNum + fDiff))
 
     def computeFrameDifference(self, fOne, fTwo):
         """
@@ -77,13 +126,22 @@ class ImageFilter:
 
         :param fOne: Integer number of the first frame.
         :param fTwo: Integer number of the second frame.
-        :return: An image in openCV format.
+        :return: The difference of two images in OpenCV format.
         """
-        img1 = cv2.imread(self.generateFramePath(fOne))
-        img2 = cv2.imread(self.generateFramePath(fTwo))
+        img1 = cv2.imread(self._generateFramePath(fOne))
+        img2 = cv2.imread(self._generateFramePath(fTwo))
         return cv2.absdiff(img1, img2)
 
-    def generateFramePath(self, fNum):
+    def readFrame(self, fNum):
+        """
+        Reads the worm image for the given frame number using OpenCV read.
+
+        :param f: The frame number to be read.
+        :return: An image in OpenCV format.
+        """
+        return cv2.imread(self._generateFramePath(fNum))
+
+    def _generateFramePath(self, fNum):
         """
         Generates the absolute path to a given frame number in the 2D images
         directory for C. Elegans. This requires the frames to be named
@@ -110,7 +168,11 @@ if __name__ == "__main__":
     fStart = 1
     fDiff = 10
     fEnd = 680
-    fPause = 1      # in milliseconds
+    fPause = 40      # in milliseconds
 
     imageFilter = ImageFilter(fStart, fEnd, fDiff, fPause)
     imageFilter.runAnimation()
+
+# TODO:
+# 1. Plot the normal animation of the worm next to subtracted frames
+# 2. Plot the histogram of intensities next to that as well
